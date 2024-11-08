af0aff1c14bbfe3b85c5009477125a07400463db
myklss <myclsky@gmail.com>
2024-11-08 13:38:53 +0800

    tcping检测工具
    tcping检测工具

---
+++ Form1.Designer.cs
@@ -0,0 +1,148 @@
+﻿
+namespace tcping
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.tcping = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.IP = new System.Windows.Forms.TextBox();
+            this.Port = new System.Windows.Forms.TextBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.continuousPingCheckBox = new System.Windows.Forms.CheckBox();
+            this.Topdisplay = new System.Windows.Forms.CheckBox();
+            this.SuspendLayout();
+            // 
+            // tcping
+            // 
+            this.tcping.Location = new System.Drawing.Point(244, 12);
+            this.tcping.Name = "tcping";
+            this.tcping.Size = new System.Drawing.Size(75, 23);
+            this.tcping.TabIndex = 0;
+            this.tcping.Text = "开始";
+            this.tcping.UseVisualStyleBackColor = true;
+            this.tcping.Click += new System.EventHandler(this.tcping_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(18, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(23, 12);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "IP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(153, 17);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 12);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "端口:";
+            // 
+            // IP
+            // 
+            this.IP.Location = new System.Drawing.Point(47, 14);
+            this.IP.Name = "IP";
+            this.IP.Size = new System.Drawing.Size(100, 21);
+            this.IP.TabIndex = 3;
+            // 
+            // Port
+            // 
+            this.Port.Location = new System.Drawing.Point(193, 14);
+            this.Port.Name = "Port";
+            this.Port.Size = new System.Drawing.Size(45, 21);
+            this.Port.TabIndex = 4;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(20, 77);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(299, 208);
+            this.listBox1.TabIndex = 5;
+            // 
+            // continuousPingCheckBox
+            // 
+            this.continuousPingCheckBox.AutoSize = true;
+            this.continuousPingCheckBox.Location = new System.Drawing.Point(47, 55);
+            this.continuousPingCheckBox.Name = "continuousPingCheckBox";
+            this.continuousPingCheckBox.Size = new System.Drawing.Size(72, 16);
+            this.continuousPingCheckBox.TabIndex = 6;
+            this.continuousPingCheckBox.Text = "持续PING";
+            this.continuousPingCheckBox.UseVisualStyleBackColor = true;
+            // 
+            // Topdisplay
+            // 
+            this.Topdisplay.AutoSize = true;
+            this.Topdisplay.Location = new System.Drawing.Point(193, 55);
+            this.Topdisplay.Name = "Topdisplay";
+            this.Topdisplay.Size = new System.Drawing.Size(72, 16);
+            this.Topdisplay.TabIndex = 7;
+            this.Topdisplay.Text = "顶层显示";
+            this.Topdisplay.UseVisualStyleBackColor = true;
+            this.Topdisplay.CheckedChanged += new System.EventHandler(this.Topdisplay_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(337, 303);
+            this.Controls.Add(this.Topdisplay);
+            this.Controls.Add(this.continuousPingCheckBox);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.Port);
+            this.Controls.Add(this.IP);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tcping);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "tcping检测工具";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button tcping;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox IP;
+        private System.Windows.Forms.TextBox Port;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox continuousPingCheckBox;
+        private System.Windows.Forms.CheckBox Topdisplay;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.Threading;
+using System.Net.Sockets;
+
+
+namespace tcping
+{
+    public partial class Form1 : Form
+    {
+        private CancellationTokenSource cts;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void tcping_Click(object sender, EventArgs e)
+        {
+            if (tcping.Text == "开始")
+            {
+                string ip = IP.Text;
+                if (!int.TryParse(Port.Text, out int port))
+                {
+                    MessageBox.Show("请输入有效的端口号。", "输入错误", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    return;
+                }
+
+                tcping.Text = "停止";
+                continuousPingCheckBox.Enabled = false;
+                cts = new CancellationTokenSource();
+
+                try
+                {
+                    listBox1.Items.Clear();
+
+                    while (!cts.Token.IsCancellationRequested)
+                    {
+                        long latency = await TcpPingAsync(ip, port, cts.Token);
+                        string result;
+                        if (latency >= 0)
+                        {
+                            result = $"连接 {ip}:{port} 成功 延迟:{latency}ms";
+                        }
+                        else
+                        {
+                            result = $"连接 {ip}:{port} 失败 Timeout";
+                        }
+
+                        listBox1.Items.Add(result);
+                        listBox1.SelectedIndex = listBox1.Items.Count - 1;
+
+                        if (!continuousPingCheckBox.Checked)
+                        {
+                            break;
+                        }
+
+                        await Task.Delay(1000, cts.Token);
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    // Operation was cancelled, do nothing
+                }
+                catch (Exception ex)
+                {
+                    listBox1.Items.Add($"Error: {ex.Message}");
+                }
+                finally
+                {
+                    cts.Dispose();
+                    cts = null;
+                    tcping.Text = "开始";
+                    continuousPingCheckBox.Enabled = true;
+                }
+            }
+            else
+            {
+                cts?.Cancel();
+            }
+        }
+
+
+        private async Task<long> TcpPingAsync(string host, int port, CancellationToken cancellationToken)
+        {
+            using (var tcpClient = new TcpClient())
+            {
+                var stopwatch = System.Diagnostics.Stopwatch.StartNew();
+
+                try
+                {
+                    var connectTask = tcpClient.ConnectAsync(host, port);
+                    var timeoutTask = Task.Delay(TimeSpan.FromSeconds(5), cancellationToken);
+
+                    var completedTask = await Task.WhenAny(connectTask, timeoutTask);
+
+                    if (completedTask == connectTask)
+                    {
+                        // Connection succeeded
+                        await connectTask; // Ensure any exceptions are propagated
+                        stopwatch.Stop();
+                        return stopwatch.ElapsedMilliseconds;
+                    }
+                    else
+                    {
+                        // Timeout occurred
+                        return -1;
+                    }
+                }
+                catch (OperationCanceledException)
+                {
+                    return -1;
+                }
+                catch (Exception)
+                {
+                    return -1;
+                }
+                finally
+                {
+                    stopwatch.Stop();
+                    tcpClient.Close();
+                }
+            }
+        }
+
+
+        private void Topdisplay_CheckedChanged(object sender, EventArgs e)
+        {
+            this.TopMost = Topdisplay.Checked;
+        }
+    }
+}
